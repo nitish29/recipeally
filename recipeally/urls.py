771648013159f1ea071cf46850d044146fc3dc46
Archit Verma <archit017@gmail.com>
2015-11-26 18:42:21 -0500

    registration backend setup

--- recipeally/urls.py
+++ recipeally/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'recipesearch.views.home', name='home'),
+    url(r'^registration/', 'recipeally.views.registration', name='registration')
     #url('^', include('django.contrib.auth.urls')),
     #url(r'^login/$', 'django.contrib.auth.views.login'),
     #url(r'^logout/$', 'django.contrib.auth.views.logout'),
