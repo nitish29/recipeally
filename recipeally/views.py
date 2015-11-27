20a7f57ea456307dab2eb04e3db6ee3541ffac47
Archit Verma <archit017@gmail.com>
2015-11-27 17:56:17 -0500

    missing files added

---
+++ recipeally/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .forms import SignUpForm
+from .forms import ProfileForm
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.db.models.signals import post_save
+
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import logout
+def registration(request):
+	title = 'Welcome'
+	# if request.user.is_authenticated():
+	# 	title="My Title %s" %(request.user)
+	# print request
+	# if request.method=="POST":
+	# 	print(request.POST)
+	form=SignUpForm(request.POST or None)
+	form2=ProfileForm(request.POST or None)
+	context={
+		"title": title,
+		"form": form,
+		"form2":form2
+	}
+
+	if form.is_valid():
+		if form2.is_valid():
+			instance=form.save(commit=False)
+			instance.save()
+			instance2=form2.save(commit=False)
+			instance2.user=instance
+			instance2.save()
+	
+			context={
+				"title": "Thank You"
+			}
+	return render(request, "registration.html",context)
+
+def user_login(request):
+    # Like before, obtain the context for the user's request.
+    context = RequestContext(request)
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            #print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render_to_response('login.html', {}, context)
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/')
