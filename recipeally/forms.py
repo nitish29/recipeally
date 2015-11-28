f118f0f2e97889d63fef36e156d21f1e25572146
Archit Verma <archit017@gmail.com>
2015-11-27 21:14:51 -0500

    forms added

---
+++ recipeally/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from recipesearch.models import User
+from recipesearch.models import UserProfile
+class SignUpForm(forms.ModelForm):
+	class Meta:
+		model=User
+		fields=['username','first_name','last_name','email','password']
+
+	def clean_email(self):
+		email=self.cleaned_data.get('email')
+		email_base,provider=email.split("@")
+		domain,extension=provider.split('.')
+		if not extension == "edu":
+			raise forms.ValidationError("Use edu address")
+		return email
+
+class ProfileForm(forms.ModelForm):
+	class Meta:
+		model=UserProfile
+		fields=['user_about']
